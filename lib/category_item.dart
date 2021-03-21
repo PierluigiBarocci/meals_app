5f6b4d30760f527597cbc83596b8090f8a9960e9
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-03-21 11:42:50 +0100

    add Fonts

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -14,6 +14,20 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
+      child: Text(
+        title,
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import '../dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
@@ -7,13 +9,24 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  title: catData.title,
+                  color: catData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
