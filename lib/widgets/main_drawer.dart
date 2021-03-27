34ee255cf1fc6b068a4e57469aa97c8435b6a1d0
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-03-27 09:58:18 +0100

    add Drawer

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            color: Theme.of(context).accentColor,
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTile(
+            icon: Icons.restaurant,
+            title: 'Meals',
+            onTap: () {},
+          ),
+          CustomTile(
+            icon: Icons.settings,
+            title: 'Filters',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTile extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Function onTap;
+  const CustomTile({
+    Key key,
+    this.title,
+    this.icon,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        // style: Theme.of(context).textTheme.headline6,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+}
