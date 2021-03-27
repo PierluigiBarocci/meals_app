767f20a064b6afb35de29934737af676c7484250
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-03-27 15:45:52 +0100

    add plastic Filters

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'package:meals_app/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
   const FiltersScreen({Key key}) : super(key: key);
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +23,87 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Your Filters'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                CustomSwitchTile(
+                  title: 'Gluten-free',
+                  description: 'Only show gluten-free meals',
+                  currentValue: _glutenFree,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                CustomSwitchTile(
+                  title: 'Vegetarian',
+                  description: 'Only show vegetarian meals',
+                  currentValue: _vegetarian,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                CustomSwitchTile(
+                  title: 'Vegan',
+                  description: 'Only show vegan meals',
+                  currentValue: _vegan,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+                CustomSwitchTile(
+                  title: 'Lactose-free',
+                  description: 'Only show lactosen-free meals',
+                  currentValue: _lactoseFree,
+                  updateValue: (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class CustomSwitchTile extends StatelessWidget {
+  final String title;
+  final String description;
+  final bool currentValue;
+  final Function updateValue;
+  const CustomSwitchTile({
+    Key key,
+    this.title,
+    this.description,
+    this.currentValue,
+    this.updateValue,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
     );
   }
 }
